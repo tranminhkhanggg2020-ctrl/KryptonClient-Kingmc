a9d5dd6c56423c08ea7779a7abb98c582b807193
tranminhkhanggg2020-ctrl <tranminhkhanggg2020@gmail.com>
2026-05-28 12:47:43 +0700

    Create SusChunkFinder.java

---
+++ src/main/java/skid/krypton/module/modules/donut/SusChunkFinder.java
@@ -0,0 +1,22 @@
+package skid.krypton.module.modules.donut;
+
+import skid.krypton.module.Category;
+import skid.krypton.module.Module;
+
+public class SusChunkFinder extends Module {
+
+    public SusChunkFinder() {
+        // Tên module, danh mục và mô tả hiển thị trong giao diện ClickGUI
+        super("SusChunkFinder", Category.DONUT, "Phat hien chunk kha nghi de tim base.");
+    }
+
+    @Override
+    public void onEnable() {
+        // Nơi chứa logic khi bạn bật module
+    }
+
+    @Override
+    public void onDisable() {
+        // Nơi chứa logic khi tắt module
+    }
+}
